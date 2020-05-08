d72271a7b50f4c1fa2743c0cfec4fbeac7402dc4
Castor Verhoog <c.a.verhoog@student.tudelft.nl>
2020-05-08 10:21:55 +0200

    add deploy message and expose container

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,7 @@ ARG release_mode
 COPY deploy.php /deploy.php
 COPY freemius-php-api /freemius-php-api
 
+EXPOSE 80/tcp
+EXPOSE 80/udp
+
 RUN php ./deploy.php ${file_name} ${version} ${sandbox} ${release_mode}
--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
     // run as: php ./deploy.php $file_name $version $sandbox $release_mode
     // with env vars: USER_ID, PUBLIC_KEY, SECRET_KEY, PLUGIN_SLUG
-    echo $_ENV['PLUGIN_SLUG'];
+    echo "\n- Deploying " . str($_ENV['PLUGIN_SLUG']) . " to Freemius";
 
     require_once 'freemius-php-api/freemius/FreemiusBase.php';
     require_once 'freemius-php-api/freemius/Freemius.php';
