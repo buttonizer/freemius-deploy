6d0f43680389405a4cc6e105e35893503c7127b3
Castor Verhoog <c.a.verhoog@student.tudelft.nl>
2020-05-08 11:06:15 +0200

    fix vars

--- deploy.php
+++ deploy.php
@@ -1,13 +1,16 @@
 <?php
     // run as: php /deploy.php $file_name $version $sandbox $release_mode
     // with env vars: USER_ID, PUBLIC_KEY, SECRET_KEY, PLUGIN_SLUG
+    $file_name = $_ENV['INPUT_FILE_NAME'];
+    $version = $_ENV['INPUT_VERSION'];
+    $sandbox = ($_ENV['INPUT_SANDBOX'] === 'true' );
+    $release_mode = ! isset( $_ENV['INPUT_RELEASEMODE'] ) || empty( $_ENV['INPUT_RELEASEMODE'] ) ? 'pending' :  $_ENV['INPUT_RELEASEMODE'];
+
     echo "\n- Deploying " . $_ENV['PLUGIN_SLUG'] . " to Freemius, with arguments: ";
-    echo "\n- file_name: " . $argv[1] . " version: " . $argv[2] . " sandbox: " . $argv[3] . " release_mode: " . $argv[4];
+    echo "\n- file_name: " . $file_name . " version: " . $version . " sandbox: " . $sandbox . " release_mode: " . $release_mode;
 
     require_once 'freemius-php-api/freemius/FreemiusBase.php';
     require_once 'freemius-php-api/freemius/Freemius.php';
-	$sandbox      = ( $argv[3] === 'true' );
-	$release_mode = ! isset( $argv[4] ) || empty( $argv[4] ) ? 'pending' :  $argv[4];
 	define( 'FS__API_SCOPE', 'developer' );
 	define( 'FS__API_DEV_ID', $_ENV['DEV_ID'] );
 	define( 'FS__API_PUBLIC_KEY', $_ENV['PUBLIC_KEY'] );
@@ -25,7 +28,7 @@
         }
 
         $deploy = $api->Api('plugins/'.$_ENV['PLUGIN_SLUG'].'/tags.json');
-        if ( $deploy->tags[0]->version === $argv[2] ) {
+        if ( $deploy->tags[0]->version === $version ) {
                 $deploy = $deploy->tags[0];
                 echo '-Package already deployed on Freemius'."\n";
         } else {
@@ -33,7 +36,7 @@
             $deploy = $api->Api('plugins/'.$_ENV['PLUGIN_SLUG'].'/tags.json', 'POST', array(
                 'add_contributor' => false
             ), array(
-                'file' => $argv[1]
+                'file' => $file_name
             ));
 
             if (!property_exists($deploy, 'id')) {
@@ -55,8 +58,8 @@
         // Generate url to download the zip
         $zip = $api->GetSignedUrl('plugins/'.$_ENV['PLUGIN_SLUG'].'/tags/'.$deploy->id.'.zip');
 
-        $path = pathinfo($argv[1]);
-        $newzipname = $path['dirname'] . '/' . basename($argv[1], '.zip');
+        $path = pathinfo($file_name);
+        $newzipname = $path['dirname'] . '/' . basename($file_name, '.zip');
         $newzipname .= '__free.zip';
 
         file_put_contents($newzipname,file_get_contents($zip));
